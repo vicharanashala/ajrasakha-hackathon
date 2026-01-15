c8914f55e5c0c018888439c055c65978d3ccbbac
Bibin T <bibint2918@gmail.com>
2026-01-15 16:06:43 +0530

    chore: create .github folder on root

--- hackathon-site/src/pages/index.js
+++ hackathon-site/src/pages/index.js
@@ -2,7 +2,6 @@
 import CipherThon from "../components/Shared/Landing1"
 import HackathonSchedule from '../components/Shared/Landing2'
 import PrizeCards from '../components/Shared/Landing3'
-import CipherNavbar from '../components/Shared/Navbar'
 import CipherLayout from '../components/Layout'
 
 export default function Page() {
