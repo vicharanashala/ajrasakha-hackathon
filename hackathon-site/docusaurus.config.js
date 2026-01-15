292a9aeb4cf42eab01d0ceb347f86c24ce443543
Abiram-k <abiramk0107@gmail.com>
2026-01-15 14:58:44 +0530

    feat: add doployment configs

--- hackathon-site/docusaurus.config.js
+++ hackathon-site/docusaurus.config.js
@@ -4,7 +4,7 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://vicharanashala.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/<repo-name>/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'vicharanashala', // Usually your GitHub org/user name.
+  projectName: 'ajrasakha-hackathon', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -81,74 +81,9 @@ const config = {
       image: 'img/docusaurus-social-card.jpg',
       colorMode: {
         // respectPrefersColorScheme: true,
-         defaultMode: 'light',
+        defaultMode: 'light',
       },
-      // navbar: {
-      //   title: 'My Site',
-      //   logo: {
-      //     alt: 'My Site Logo',
-      //     src: 'img/logo.svg',
-      //   },
-      //   items: [
-      //     {
-      //       type: 'docSidebar',
-      //       sidebarId: 'tutorialSidebar',
-      //       position: 'left',
-      //       label: 'Tutorial',
-      //     },
-      //     {to: '/blog', label: 'Blog', position: 'left'},
-      //     {
-      //       href: 'https://github.com/facebook/docusaurus',
-      //       label: 'GitHub',
-      //       position: 'right',
-      //     },
-      //   ],
-      // },
-      // footer: {
-      //   style: 'dark',
-      //   links: [
-      //     {
-      //       title: 'Docs',
-      //       items: [
-      //         {
-      //           label: 'Tutorial',
-      //           to: '/docs/intro',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'Community',
-      //       items: [
-      //         {
-      //           label: 'Stack Overflow',
-      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //         },
-      //         {
-      //           label: 'Discord',
-      //           href: 'https://discordapp.com/invite/docusaurus',
-      //         },
-      //         {
-      //           label: 'X',
-      //           href: 'https://x.com/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'More',
-      //       items: [
-      //         {
-      //           label: 'Blog',
-      //           to: '/blog',
-      //         },
-      //         {
-      //           label: 'GitHub',
-      //           href: 'https://github.com/facebook/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      // },
+
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
