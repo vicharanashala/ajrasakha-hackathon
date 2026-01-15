8d442a3c560d48514affc6509643c3b29eaa9c4d
Bibin T <bibint2918@gmail.com>
2026-01-15 19:35:49 +0530

    fix: showed annam logo on url

--- hackathon-site/docusaurus.config.js
+++ hackathon-site/docusaurus.config.js
@@ -82,8 +82,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/Annam-logo-icon.png',
       colorMode: {
         // respectPrefersColorScheme: true,
         defaultMode: 'light',
